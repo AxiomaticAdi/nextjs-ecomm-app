d212b96bfd7d51339e4141da3215c53a615717b6
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-10-13 19:36:40 -0700

    Logo added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import NavBar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -17,6 +18,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
+                <NavBar />
                 <main className="m-auto max-w-7xl p-4">{children}</main>
             </body>
         </html>
