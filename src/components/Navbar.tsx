b8ba554746b078a6370506a245c3c890ff4bcba4
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-10-15 00:10:01 -0700

    Logo change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import ShoppingCartButton from "./ShoppingCartButton";
-import logo from "../assets/spooky_paws.png";
+import logo from "../assets/spooky-paws-2.png";
 import { getCart } from "@/lib/db/cart";
 
 export default async function NavBar() {
@@ -11,11 +11,12 @@ export default async function NavBar() {
         <div className="h-28 bg-primary">
             <div className="navbar m-auto flex max-w-7xl flex-col gap-5 sm:flex-row">
                 <div className="flex-1 justify-between">
-                    <Image src={logo} alt={"Spooky Paws Logo"} height={100} />
                     <Link href={"/"} className="btn btn-ghost h-24">
-                        <h1 className="text-5xl normal-case text-black">
-                            Spooky Paws
-                        </h1>
+                        <Image
+                            src={logo}
+                            alt={"Spooky Paws Logo"}
+                            height={100}
+                        />
                     </Link>
                     <ShoppingCartButton cart={currentCart} />
                 </div>
