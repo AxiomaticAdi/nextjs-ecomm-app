c83af3c78d3bb97b01967c9af9b5bad8428b8ea2
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-10-15 09:48:17 -0700

    Added total and checkout button

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,13 +1,17 @@
 import CartCheckoutEntry from "@/components/CartCheckoutEntry";
+import ModalButton from "@/components/ModalButton";
 import { getCart } from "@/lib/db/cart";
 
 export default async function CartPage() {
     const cart = await getCart();
 
+    const checkoutText =
+        "We hope you enjoyed browsing our spook-tacular pet costumes. Just a little note: this site was created as a project to delve deeper into web development. Checkout functionality has been disabled, so no real transactions will occur. Thanks for understanding and for your continued support on this learning journey!";
+
     return (
         <div>
             <div>
-                <h1 className="mb-6 text-center text-3xl font-bold">
+                <h1 className="my-6 text-center text-3xl font-bold">
                     Shopping Cart
                 </h1>
             </div>
@@ -15,6 +19,16 @@ export default async function CartPage() {
                 {cart?.items.map((cartItem) => (
                     <CartCheckoutEntry cartItem={cartItem} key={cartItem.id} />
                 ))}
+            </div>
+            <div className="flex flex-col items-end sm:items-center">
+                <p className="my-3 font-bold">{`Total: $ ${
+                    cart?.subtotal ? cart?.subtotal : 0
+                }`}</p>
+                <ModalButton
+                    buttonText={"Checkout"}
+                    modalTitle={"Hello!"}
+                    modalContent={checkoutText}
+                />
             </div>
         </div>
     );
---
+++ src/components/ModalButton.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+interface ModalButtonProps {
+    buttonText: string;
+    modalTitle: string;
+    modalContent: string;
+}
+
+export default function ModalButton({
+    buttonText,
+    modalTitle,
+    modalContent,
+}: ModalButtonProps) {
+    return (
+        <>
+            <button
+                className="btn btn-primary"
+                onClick={() =>
+                    document.getElementById("modal_name").showModal()
+                }
+            >
+                {buttonText}
+            </button>
+            <dialog id="modal_name" className="modal">
+                <div className="modal-box">
+                    <h3 className="text-lg font-bold">{modalTitle}</h3>
+                    <p className="py-4">{modalContent}</p>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn btn-primary">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+        </>
+    );
+}
